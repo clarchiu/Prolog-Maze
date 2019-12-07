b68b9e47be1f8272b74fbaf3545a8b8208c749d4
i2p1b <i2p1b@ugrad.cs.ubc.ca>
2019-12-06 21:31:05 -0800

    added base for key presses

--- maze.pl
+++ maze.pl
@@ -1,8 +1,7 @@
 %10x10 maze
 mazeSize(10,10).
 % - open index is printed as double spaces "  "
-% - and closed is printed as double X's "XX"
-
+% - and closed is printed as double Xs "XX"
 
 maze(1, 1, open).
 maze(1, 2, closed).
@@ -120,7 +119,7 @@ print_line(Width) :-
 % main print predicate    
 print_maze :-
     mazeSize(Height, Width),
-    print_line(Width),
+    print_line(Width), nl,
     % for each line of the maze    
     forall(between(1, Height, I),
             (write('XX'),
@@ -133,3 +132,21 @@ print_maze :-
                             write(C))),
                 writeln('XX'))),
     print_line(Width).
+
+/* Reading key presses */
+
+read_key([Code|Codes]) :-
+   get_single_char(Code),
+   read_pending_codes(user,Codes,[]).
+
+read_keyatom(KAtom) :-
+    write('Press arrow key to move'),
+    read_key(Codes),
+    codes_keyatom(Codes,KAtom).
+
+codes_keyatom([27,91,65],up)    :- !.
+codes_keyatom([27,91,66],down)  :- !.
+codes_keyatom([27,91,67],right) :- !.
+codes_keyatom([27,91,68],left)  :- !.
+
+% ?- read_keyatom(Key).
