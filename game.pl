487b0abcc7438ac09ec6b76ff9a5670fa782346b
clarchiu <clarchiu@gmail.com>
2019-12-07 07:29:49 -0800

    helper functions

--- game.pl
+++ game.pl
@@ -7,46 +7,33 @@
 :- retractall(currentPos(_,_)).
 :- retractall(currentMazeSize(_,_,_)).
 
+% Game state
 currentPos(1, 1).
 currentMazeSize(big,10,10).
-
 goalPos(R,C) :-
   currentMazeSize(_,R,C).
 
-% Helper function to set currentMazeSize
-choose_maze(Size) :-
+% Helper function to initialize state
+initialize(Size, Mode) :-
+  currentPos(_,_),
+  retractall(currentPos(_,_)),
+  assert(currentPos(1,1)),
   mazeSize(Size,H,W),
   retract(currentMazeSize(_,_,_)),
-  assert(currentMazeSize(Size,H,W)).
+  assert(currentMazeSize(Size,H,W)),
+  print_maze(Size,Mode).
 
 % To start the game and reset player position
 play(Size, interactive) :-
-  currentPos(_,_),
-  retractall(currentPos(_,_)),
-  assert(currentPos(1,1)),
-  choose_maze(Size),
-  print_maze(Size,interactive),
+  initialize(Size, interactive),
   key_press(_).
 play(Size, query) :-
-  currentPos(_,_),
-  retractall(currentPos(_,_)),
-  assert(currentPos(1,1)),
-  choose_maze(Size),
-  print_maze(Size,query).
-
-% Win condition check
-check_win(R,C) :-
-  (goalPos(R,C)-> win ; true).
-
-win :-
-  nl,
-  writeln('You are a winner!'),
-  false. %false in order to end the game
+  initialize(Size, interactive).
 
 % Interactive Mode
 % - handle keyboard events
 key_press(Key) :-
-  read_keyatom(Key),
+  map_key(Key),
     (Key == up -> move(up, interactive) ;
       Key == right -> move(right, interactive) ;
         Key == down -> move(down, interactive) ;
@@ -67,6 +54,22 @@ r :-
   currentMazeSize(Size,_,_),
   play(Size,query).
 
+% Win condition check
+check_win(R,C) :-
+  (goalPos(R,C)-> win ; true).
+
+win :-
+  nl,
+  writeln('You are a winner!'),
+  false. %false in order to end the game
+
+% Walkable check_win
+walkable(Size,R,C) :-
+  currentMazeSize(Size,H,W),
+  R =< W, R > 0,
+  C =< H, C > 0,
+  maze(Size,R,C,open).
+
 % Helper function to update position
 update(R,C,R1,C1,Mode) :-
   retract(currentPos(R,C)),
@@ -83,8 +86,8 @@ move(right, interactive) :-
   currentMazeSize(Size,_,W),
   C1 is C + 1,
   ((maze(Size, R, C1, open), C1 =< W) ->  % prolog if-then-else
-      update(R,C,R,C1,interactive);
-      true),
+    update(R,C,R,C1,interactive);
+    true),
   key_press(_). % wait for next keyboard event
 move(right, query) :-
   currentPos(R, C),
--- key.pl
+++ key.pl
@@ -1,39 +1,26 @@
-read_key([Code|Codes]) :-
+% Handler for keyboard events
+
+% reads a character from keyboard input
+get_key([Code|Codes]) :-
    get_single_char(Code),
    read_pending_codes(user,Codes,[]).
 
-read_keyatom(KAtom) :-
-    read_key(Codes),
+% maps prolog character representation to program specific atoms
+map_key(KAtom) :-
+    get_key(Codes),
     ((Codes == [27,91,65];
       Codes == [27,91,66];
       Codes == [27,91,67];
       Codes == [27,91,68];
       Codes == [113];
       Codes == [114]) ->
-        codes_keyatom(Codes, KAtom) ;
+        key_to_atom(Codes, KAtom) ;
         %(writeln('Not a valid key pressed')),
-        read_keyatom(KAtom)).
+        map_key(KAtom)).
 
-codes_keyatom([27,91,65],up)    :- !.
-codes_keyatom([27,91,66],down)  :- !.
-codes_keyatom([27,91,67],right) :- !.
-codes_keyatom([27,91,68],left)  :- !.
-codes_keyatom([113],quit)       :- !.
-codes_keyatom([114],reset)      :- !.
-
-% read_keyatom(KAtom) :-
-%   read_key(Codes),
-%   ((Codes == [27,91,65]; Codes == [27,91,66] ; Codes == [27,91,67] ; Codes == [27,91,68]) ->
-%     codes_keyatom(Codes, KAtom)).
-% read_keyatom(KAtom) :-
-%   read_key(Codes),
-%   Codes == [113],
-%   codes_keyatom(Codes, KAtom).
-% read_keyatom(KAtom) :-
-%   read_key(Codes),
-%   Codes == [114],
-%   codes_keyatom(Codes, KAtom).
-% read_keyatom(KAtom) :-
-%   read_key(Codes),
-%   Codes == [_],
-%   read_keyatom(KAtom).
+key_to_atom([27,91,65],up)    :- !.
+key_to_atom([27,91,66],down)  :- !.
+key_to_atom([27,91,67],right) :- !.
+key_to_atom([27,91,68],left)  :- !.
+key_to_atom([113],quit)       :- !.
+key_to_atom([114],reset)      :- !.
