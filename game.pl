258e3bdeacdd7c9d02c5080a8115daac0247f235
clarchiu <clarchiu@gmail.com>
2019-12-07 02:04:10 -0800

    Update game.pl

--- game.pl
+++ game.pl
@@ -14,48 +14,47 @@ play :-
   print_maze,
   key_press(_).
 
+% move(right) :-
+%   currentPos(R, C),
+%   check_win(R,C),
+%   C1 is C + 1,
+%   ((maze(R, C1, open), C1 < 11) ->
+%       retract(currentPos(R,C)),
+%       assert(currentPos(R,C1)),
+%       print_maze ;
+%       true),
+%   key_press(_).
+%
+% move(left) :-
+%   currentPos(R, C),
+%   C1 is C - 1,
+%   ((maze(R, C1, open), C1 > 0) ->
+%     retract(currentPos(R,C)),
+%     assert(currentPos(R,C1)),
+%     print_maze ;
+%     true),
+%   key_press(_).
+%
+% move(up) :-
+%   currentPos(R, C),
+%   R1 is R - 1,
+%   ((maze(R1, C, open), R1 > 0) ->
+%     retract(currentPos(R,C)),
+%     assert(currentPos(R1,C)),
+%     print_maze ;
+%     true),
+%   key_press(_).
+%
+% move(down) :-
+%   currentPos(R, C),
+%   R1 is R + 1,
+%   ((maze(R1, C, open), R1 < 11) ->
+%     retract(currentPos(R,C)),
+%     assert(currentPos(R1,C)),
+%     print_maze ;
+%     true),
+%   key_press(_).
 
-move(right) :-
-  currentPos(R, C),
-  check_win(R,C),
-  C1 is C + 1,
-  ((maze(R, C1, open), C1 < 11) ->
-      retract(currentPos(R,C)),
-      assert(currentPos(R,C1)),
-      print_maze ;
-      true),
-  key_press(_).
-
-move(left) :-
-  currentPos(R, C),
-  C1 is C - 1,
-  ((maze(R, C1, open), C1 > 0) ->
-    retract(currentPos(R,C)),
-    assert(currentPos(R,C1)),
-    print_maze ;
-    true),
-  key_press(_).
-
-move(up) :-
-  currentPos(R, C),
-  R1 is R - 1,
-  ((maze(R1, C, open), R1 > 0) ->
-    retract(currentPos(R,C)),
-    assert(currentPos(R1,C)),
-    print_maze ;
-    true),
-  key_press(_).
-
-move(down) :-
-  currentPos(R, C),
-  R1 is R + 1,
-  ((maze(R1, C, open), R1 < 11) ->
-    retract(currentPos(R,C)),
-    assert(currentPos(R1,C)),
-    print_maze ;
-    true),
-  key_press(_).
-/*
 move(right) :-
   currentPos(R, C),
   check_win(R,C),
@@ -106,7 +105,7 @@ move(up) :-
   key_press(_).
 move(up) :-
   key_press(_).
-*/
+
 
 
 check_win(R,C) :-
