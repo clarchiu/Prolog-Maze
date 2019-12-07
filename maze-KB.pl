1d221482b1248620d457e024e884437398c45ec2
clarence <clarchiu@gmail.com>
2019-12-06 22:59:30 -0800

    Update maze-KB.pl

--- maze-KB.pl
+++ maze-KB.pl
@@ -105,41 +105,23 @@ maze(10, 8, closed).
 maze(10, 9, open).
 maze(10, 10, open).
 
-:- dynamic currentPos/2.
-:- retractall(currentPos(_,_)).
-
-currentPos(1, 1).
-
-play :-
-  currentPos(_,_),
-  retractall(currentPos(_,_)),
-  assert(currentPos(1,1)).
-
-move(right) :-
-  currentPos(R, C),
-  maze(R, C1, open),
-  C1 is C+1,
-  C1 < 11,
-  retract(currentPos(R,C)),
-  assert(currentPos(R,C1)).
-
 /* Print predicate for maze */
 
 % char to represented open/closed
 getChar(open, '  ').
 getChar(closed, 'XX').
 
-% first and last lines of the maze    
+% first and last lines of the maze
 print_line(Width) :-
     write('XX'),
     forall(between(1,Width, _), write('XX')),
     writeln('XX').
 
-% main print predicate    
+% main print predicate
 print_maze :-
     mazeSize(Height, Width),
     print_line(Width),
-    % for each line of the maze    
+    % for each line of the maze
     forall(between(1, Height, I),
             (write('XX'),
                 % for each cell of the line
